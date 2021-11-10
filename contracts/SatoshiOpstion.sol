330da118760052c59d001c8a99f9745aa673ddb1
zhangyi999 <zhang_btc@outlook.com>
2021-11-10 20:01:54 +0800

    fix

--- contracts/SatoshiOpstion.sol
+++ contracts/SatoshiOpstion.sol
@@ -8,6 +8,9 @@ contract SatoshiOpstion is ERC721, Ownable {
     // 筹码
     address public cppc;
 
+    // 仓位id
+    uint256 private _totalSupply;
+
     // 用户的 nft 列表
     // user => [ids]
     mapping(address => uint256[]) private _idBalance;
@@ -21,7 +24,7 @@ contract SatoshiOpstion is ERC721, Ownable {
         string direction;
         bool isEnable;
     }
-    mapping(address => NftData) nftStore;
+    mapping(uint256 => NftData) nftStore;
 
     constructor(
         address _cppc,
@@ -29,6 +32,10 @@ contract SatoshiOpstion is ERC721, Ownable {
         string memory symbol_
     ) ERC721(name_, symbol_) {
         cppc = _cppc;
+    }
+
+    function totalSupply() external view returns(uint256) {
+        return _totalSupply;
     }
 
     function setCppc(address _cppc) external onlyOwner {
@@ -52,21 +59,14 @@ contract SatoshiOpstion is ERC721, Ownable {
         }
     }
 
-    // 记录 id
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override {
-        _idBalance[to].push(tokenId);
-    }
 
-    function nftMint(uint256 tokenId, NftData memory _nftData)
+    // 开仓
+    function open(NftData memory _nftData)
         public
-        returns (uint256)
+        returns (uint256 pid)
     {
-        super._mint(_nftData._address, tokenId);
-        NftData storage nftData = nftStore[_nftData._address];
+        pid = _mintNft(_msgSender());
+        NftData storage nftData = nftStore[pid];
         nftData._address = _nftData._address;
         nftData.lever = _nftData.lever;
         nftData.cppcNum = _nftData.cppcNum;
@@ -74,6 +74,20 @@ contract SatoshiOpstion is ERC721, Ownable {
         nftData.openPrice = _nftData.openPrice;
         nftData.direction = _nftData.direction;
         nftData.isEnable = true;
-        return tokenId;
+    }
+
+    function _mintNft(address _to) internal returns(uint256) {
+        _mint(_to, _totalSupply);
+        return _totalSupply+=1;
+    } 
+
+
+    // 记录 id
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        _idBalance[to].push(tokenId);
     }
 }
