4aca286db998a4d02cb317d7befcfd0b67c9378e
DY Ye <yedongyan@gmail.com>
2021-12-19 17:16:32 +0800

    add more contracts

---
+++ contracts/cbbc_related/ExternCbbcStateToken.sol
@@ -0,0 +1,215 @@
+pragma solidity 0.8.3;
+
+// Inheritance
+import "./Owned.sol";
+import "./Proxyable.sol";
+import "./TokenState.sol";
+
+// Libraries
+//import "./libraries/SafeDecimalMath.sol";
+
+// Internal references
+interface IExternCbbcStateToken{
+    function approve(address spender, uint value) external returns (bool);
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    // Views
+    function totalSupply() external view returns (uint);
+
+}
+
+// https://docs.synthetix.io/contracts/source/contracts/externstatetoken
+contract ExternCbbcStateToken is Owned, Proxyable, IExternCbbcStateToken {
+//    using SafeMath for uint;
+//    using SafeDecimalMath for uint;
+
+    /* ========== STATE VARIABLES ========== */
+    mapping(address => uint) public nonces;
+
+    /* Stores balances and allowances. */
+    TokenState public tokenState;
+
+    bytes32 public DOMAIN_SEPARATOR;
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    /* Other ERC20 fields. */
+    string public name;
+    string public symbol;
+    uint public override totalSupply;
+    uint internal gonsPerFragment;
+    uint8 public decimals;
+
+    constructor(
+        address payable _proxy,
+        TokenState _tokenState,
+        string memory _name,
+        string memory _symbol,
+        uint _totalSupply,
+        uint _gonsPerFragment,
+        uint8 _decimals,
+        address _owner
+    ) Owned(_owner) Proxyable(_proxy) {
+        require(_proxy != address(0), "_proxy cannot be 0");
+        require(_owner != address(0), "_owner cannot be 0");
+
+        tokenState = _tokenState;
+
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        gonsPerFragment = _gonsPerFragment;
+        decimals = _decimals;
+    }
+
+    /* ========== VIEWS ========== */
+
+    /**
+     * @notice Returns the ERC20 allowance of one party to spend on behalf of another.
+     * @param owner The party authorising spending of their funds.
+     * @param spender The party spending tokenOwner's funds.
+     */
+    function allowance(address owner, address spender) public view returns (uint) {
+        return tokenState.allowance(owner, spender);
+    }
+
+    /**
+     * @notice Returns the ERC20 token balance of a given account.
+     */
+    function balanceOf(address account) public view returns (uint) {
+        return tokenState.balanceOf(account)/gonsPerFragment;
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /**
+     * @notice Set the address of the TokenState contract.
+     * @dev This can be used to "pause" transfer functionality, by pointing the tokenState at 0x000..
+     * as balances would be unreachable.
+     */
+    function setTokenState(TokenState _tokenState) external optionalProxy_onlyOwner {
+        tokenState = _tokenState;
+        emitTokenStateUpdated(address(_tokenState));
+    }
+
+    function _internalTransfer(
+        address from,
+        address to,
+        uint value
+    ) internal returns (bool) {
+        /* Disallow transfers to irretrievable-addresses. */
+        require(to != address(0) && to != address(this) && to != address(proxy), "Cannot transfer to this address");
+
+        // Insufficient balance will be handled by the safe subtraction.
+        uint gons = value * gonsPerFragment;
+        tokenState.setBalanceOf(from, tokenState.balanceOf(from) - gons);
+        tokenState.setBalanceOf(to, tokenState.balanceOf(to) + gons);
+
+        // Emit a standard ERC20 transfer event
+        emitTransfer(from, to, gons);
+
+        return true;
+    }
+
+    /**
+     * @dev Perform an ERC20 token transfer. Designed to be called by transfer functions possessing
+     * the onlyProxy or optionalProxy modifiers.
+     */
+    function _transferByProxy(
+        address from,
+        address to,
+        uint value
+    ) internal returns (bool) {
+        return _internalTransfer(from, to, value);
+    }
+
+    /*
+     * @dev Perform an ERC20 token transferFrom. Designed to be called by transferFrom functions
+     * possessing the optionalProxy or optionalProxy modifiers.
+     */
+    function _transferFromByProxy(
+        address sender,
+        address from,
+        address to,
+        uint value
+    ) internal returns (bool) {
+        /* Insufficient allowance will be handled by the safe subtraction. */
+        uint gons = value * gonsPerFragment;
+        tokenState.setAllowance(from, sender, tokenState.allowance(from, sender) - gons);
+        return _internalTransfer(from, to, gons);
+    }
+
+    /**
+     * @notice Approves spender to transfer on the message sender's behalf.
+     */
+    function approve(address spender, uint value) public override optionalProxy returns (bool) {
+        address sender = messageSender;
+
+        _approve(sender, spender, value);
+
+        return true;
+    }
+
+
+    function _approve(address sender, address spender, uint256 value) internal{
+        tokenState.setAllowance(sender, spender, value);
+
+        emitApproval(sender, spender, value);
+    }
+
+    function permit(
+                address owner,
+                address spender,
+                uint value,
+                uint deadline,
+                uint8 v,
+                bytes32 r,
+                bytes32 s)
+            external override {
+        require(deadline >= block.timestamp, 'CBBC: EXPIRED');
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'CBBC: INVALID_SIGNATURE');
+        _approve(owner, spender, value);
+    }
+
+    /* ========== EVENTS ========== */
+    function addressToBytes32(address input) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(input)));
+    }
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    bytes32 internal constant TRANSFER_SIG = keccak256("Transfer(address,address,uint256)");
+
+    function emitTransfer(
+        address from,
+        address to,
+        uint value
+    ) internal {
+        proxy._emit(abi.encode(value), 3, TRANSFER_SIG, addressToBytes32(from), addressToBytes32(to), 0);
+    }
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+    bytes32 internal constant APPROVAL_SIG = keccak256("Approval(address,address,uint256)");
+
+    function emitApproval(
+        address owner,
+        address spender,
+        uint value
+    ) internal {
+        proxy._emit(abi.encode(value), 3, APPROVAL_SIG, addressToBytes32(owner), addressToBytes32(spender), 0);
+    }
+
+    event TokenStateUpdated(address newTokenState);
+    bytes32 internal constant TOKENSTATEUPDATED_SIG = keccak256("TokenStateUpdated(address)");
+
+    function emitTokenStateUpdated(address newTokenState) internal {
+        proxy._emit(abi.encode(newTokenState), 1, TOKENSTATEUPDATED_SIG, 0, 0, 0);
+    }
+}
